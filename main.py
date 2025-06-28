5f3b205fdf8375658271505e000d11e02da3faee
AnshuChourasia <anshuchourasia23804@gmail.com>
2025-06-28 23:53:13 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-from sea_level_predictor import draw_plot
+# This entrypoint file to be used in development. Start by reading README.md
+# main.py
 
-fig = draw_plot()
-fig.savefig("sea_level_plot.png")
-print("Plot saved as sea_level_plot.png")
+from time_series_visualizer import draw_line_plot, draw_bar_plot, draw_box_plot
+
+# Draw Line Plot
+print("Generating Line Plot...")
+fig1 = draw_line_plot()
+fig1.savefig('line_plot.png')
+print("line_plot.png saved")
+
+# Draw Bar Plot
+print("Generating Bar Plot...")
+fig2 = draw_bar_plot()
+fig2.savefig('bar_plot.png')
+print("bar_plot.png saved")
+
+# Draw Box Plot
+print("Generating Box Plot...")
+fig3 = draw_box_plot()
+fig3.savefig('box_plot.png')
+print("box_plot.png saved")
+
+
+# Run unit tests automatically
+#main(module='test_module', exit=False)
